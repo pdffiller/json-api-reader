e42cd8cd502aca1cedb1ea8f9a95546997c018f1
Dmitry Scheglov <Dmitry.Scheglov@gmail.com>
2018-07-24 13:28:52 +0300

    1.2.2
    [dsch] fix for data: null case

--- src/entities.js
+++ src/entities.js
@@ -7,7 +7,7 @@ export const $type = Symbol('__$type');
 const rndId = () => Math.random().toFixed(20).slice(2);
 
 const toRelation = (value, key) => ({
-  [toCamelCase(key)]: map(value.data, ({ id }) => id)
+  [toCamelCase(key)]: map(value.data, data => data && data.id)
 });
 
 export const readEntity = ({ id, type, attributes, relationships }) => ({
